afbad40e6159b01d9bb8f277c956e187f1a1c741
h0gm4n <atte.hogman@gmail.com>
2025-11-08 17:24:00 +0200

    modify map

--- backend/world_map.py
+++ backend/world_map.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from geodatasets import get_path
 
 df = pd.read_csv('weather_data/europe_station_normals.csv')
+
+df = df.drop(df[df['country']=='DE'].index)
+df = df.drop(df[df['country']=='RU'].index)
 
 gdf = gpd.GeoDataFrame(
     df, geometry=gpd.points_from_xy(df['longitude'], df['latitude']), crs="EPSG:4326"
@@ -15,15 +17,17 @@ europe = world[world['CONTINENT'] == 'Europe']
 
 gdf = gpd.sjoin(gdf, europe[['geometry']], how='inner', predicate='within')
 
-ax = europe.plot(color="white", edgecolor="black", figsize=(8,8))
+ax = europe.plot(color="white", edgecolor="black", figsize=(12,12))
 
 # Plot points
 gdf.plot(ax=ax, color='red', markersize=5)
 
 # --- LIMIT VIEW TO EUROPE BOUNDS ---
-minx, miny, maxx, maxy = europe.total_bounds
 ax.set_xlim(-25, 45)
 ax.set_ylim(34, 75)
+
+for x, y, label in zip(gdf.geometry.x, gdf.geometry.y, gdf['name']):
+    ax.text(x + 0.02, y + 0.02, label, fontsize=6)
 
 plt.show()
 
